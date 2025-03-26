fffc795a81bdbb780d5682ab2030b540a44d69d3
Frederik Gelder <frederik.gelder@freenet.de>
2025-03-26 16:56:17 +0100

    debug output on 22

--- TeletypewriterInterface/BitReceiver.cs
+++ TeletypewriterInterface/BitReceiver.cs
@@ -8,6 +8,7 @@ namespace TeletypewriterInterface
     {
         private readonly GpioController gpio = new();
         private readonly int pin;
+        private readonly int debugPin = 22;
         private readonly double bitDuration;
         private readonly int bitCount;
         private bool isDisposed = false;
@@ -28,15 +29,21 @@ namespace TeletypewriterInterface
             readerThread.Start();
             gpio.OpenPin(pin, PinMode.InputPullDown);
             gpio.RegisterCallbackForPinValueChangedEvent(pin, PinEventTypes.Falling, OnStartBitDetected);
+
+            gpio.OpenPin(debugPin, PinMode.Output);
+            gpio.Write(debugPin, PinValue.Low);
         }
 
         void OnStartBitDetected(object sender, PinValueChangedEventArgs args)
         {
             if (!isReceiving)
             {
+                gpio.Write(debugPin, PinValue.High);
                 isReceiving = true;
                 timer.Restart();
                 readerEvent.Set();
+                Util.Sleep(bitDuration / 5);
+                gpio.Write(debugPin, PinValue.Low);
             }
         }
 
@@ -65,8 +72,12 @@ namespace TeletypewriterInterface
 
             int receivedByte = 0;
 
+            bool debug = true;
+
             for (int i = 0; i < bitCount; i++)
             {
+                gpio.Write(debugPin, debug ? PinValue.High : PinValue.Low);
+                debug = !debug;
                 if (ReadBit())
                 {
                     receivedByte |= 1 << i;
@@ -76,6 +87,7 @@ namespace TeletypewriterInterface
 
             // skip stop bits
             Util.Sleep(bitDuration * 1.5); //as we trigger on falling edge, we can exit half a bit early to be sure to catch the next startbit
+            gpio.Write(debugPin, PinValue.Low);
 
             return receivedByte;
         }
